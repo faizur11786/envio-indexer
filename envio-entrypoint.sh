ba9aaf54dea549009c5d25ceb309be6e9b3b7057
Faizur Rahman <faizur11786@gmail.com>
2024-09-03 12:38:49 +0400

    bug

--- envio-entrypoint.sh
+++ envio-entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 
-# check_hasura_health() {
-#     while true; do
-#         # Use curl to get the HTTP status code of the /healthz endpoint
-#         HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" "http://$HASURA_SERVICE_HOST:$HASURA_SERVICE_PORT/healthz")
+check_hasura_health() {
+    while true; do
+        # Use curl to get the HTTP status code of the /healthz endpoint
+        HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" "http://$HASURA_SERVICE_HOST:$HASURA_SERVICE_PORT/healthz")
 
-#         # Check if the HTTP status code is 200 (OK)
-#         if [ "$HTTP_STATUS" -eq 200 ]; then
-#             echo "Hasura service is healthy."
-#             break
-#         else
-#             echo "Waiting for Hasura service to become healthy..."
-#             sleep 5
-#         fi
-#     done
-# }
+        # Check if the HTTP status code is 200 (OK)
+        if [ "$HTTP_STATUS" -eq 200 ]; then
+            echo "Hasura service is healthy."
+            break
+        else
+            echo "Waiting for Hasura service to become healthy..."
+            sleep 5
+        fi
+    done
+}
 
 
-# check_hasura_health
+check_hasura_health
 export TUI_OFF=${TUI_OFF} # should use TUI (set to true by default in docker compose)
 
 # the migrate step below is only required if hasura is being used

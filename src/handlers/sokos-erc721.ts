213d4fae2a893847b4502e4578d7db8ef0c0982a
Faizur Rahman <faizur11786@gmail.com>
2024-09-16 13:57:20 +0400

    issue fixed

--- src/handlers/sokos-erc721.ts
+++ src/handlers/sokos-erc721.ts
@@ -41,6 +41,7 @@ SokosERC721.Transfer.handler(async ({ event, context }) => {
       attributes: JSON.stringify(metadata.attributes),
       isPhygital: Boolean(metadata.isPhygital),
       chainId: event.chainId,
+      market_id: undefined
     });
   } else {
     // transfer
